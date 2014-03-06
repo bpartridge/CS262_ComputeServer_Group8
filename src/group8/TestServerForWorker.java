8e597b09f104b21bd64f3e050159107d0c98cd24
Brenton Partridge <bapartridge@gmail.com>
2014-03-06 16:21:33 -0500

    Worker compiles?

---
+++ src/group8/TestServerForWorker.java
@@ -0,0 +1,46 @@
+package group8;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.util.UUID;
+
+import edu.harvard.cs262.ComputeServer.ComputeServer;
+import edu.harvard.cs262.ComputeServer.WorkQueue;
+import edu.harvard.cs262.ComputeServer.WorkTask;
+
+public class TestServerForWorker implements WorkQueue {
+
+  private ComputeServer workerStub;
+
+  @Override
+  public UUID registerWorker(ComputeServer workerStub) throws RemoteException {
+    this.workerStub = workerStub;
+    return null;
+  }
+
+  @Override
+  public boolean unregisterWorker(UUID workerID) throws RemoteException {
+    return true;
+  }
+  
+  public static void main(String args[]){
+    try {
+      if (System.getSecurityManager() == null) {
+        System.setSecurityManager(new SecurityManager());
+      }
+
+      TestServerForWorker server = new TestServerForWorker();
+      WorkQueue serverStub = (WorkQueue)UnicastRemoteObject.exportObject(server);
+      
+      Registry registry = LocateRegistry.getRegistry();
+      registry.bind("TestServerForWorker", serverStub);
+      
+      System.out.println("Server ready");
+    } catch (Exception e) {
+      System.err.println("Server exception: " + e.toString());
+    }
+  }
+}
+
--- src/group8/Worker.java
+++ src/group8/Worker.java
@@ -1,19 +1,43 @@
+package group8;
 
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
 
+import edu.harvard.cs262.ComputeServer.ComputeServer;
+import edu.harvard.cs262.ComputeServer.WorkQueue;
+import edu.harvard.cs262.ComputeServer.WorkTask;
 
 public class Worker implements ComputeServer {
 
-  
+  public Object sendWork(WorkTask work) throws RemoteException {
+    return work.doWork();
+  }
+
+  public boolean PingServer() throws RemoteException {
+    return true;
+  }
 
   public static void main(String args[]) {
-    String serverName = "Server";
-    Registry registry = LocateRegistry.getRegistry(args[0]);
-    WorkQueue serverStub = (WorkQueue) registry.lookup(name);
+    try {
+      if (System.getSecurityManager() == null) {
+        System.setSecurityManager(new SecurityManager());
+      }
+      
+      String serverName = "Server";
+      Registry registry = LocateRegistry.getRegistry(args[0]);
+      WorkQueue serverStub = (WorkQueue) registry.lookup(serverName);
 
-    ComputeServer myWorker = new Worker();
-    ComputeServer myWorkerStub = (ComputeServer)UnicastRemoteObject.exportObject(myWorker);
+      ComputeServer myWorker = new Worker();
+      ComputeServer myWorkerStub = (ComputeServer)UnicastRemoteObject.exportObject(myWorker);
 
-    serverStub.registerWorker(myWorkerStub);
+      serverStub.registerWorker(myWorkerStub);
+
+      System.out.println("Worker registered");
+    } catch (Exception e) {
+      System.err.println("Worker exception: " + e.toString());
+    }
   }
 
 }
