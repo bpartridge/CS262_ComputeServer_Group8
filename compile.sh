c5bc30cd0ec2bc1b8ea0464885697a75b6068232
Brenton Partridge <bapartridge@gmail.com>
2014-03-09 14:32:02 -0400

    Remove TestServerForWorker since QueuedServer works

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 (cd src && javac -g -d ../bin edu/harvard/cs262/ComputeServer/*.java group8/*.java)
-(cd bin && rmic group8.TestServerForWorker group8.QueuedServer group8.Worker)
+(cd bin && rmic group8.QueuedServer group8.Worker)
