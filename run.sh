6912fee47fb825be3d8316f7500f519bfcca8b9d
Brenton Partridge <bapartridge@gmail.com>
2014-03-25 14:15:25 -0400

    Removed files no longer used for clarity

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-(cd bin && rmiregistry &)
-(cd bin && java -Djava.security.policy=../stupid.policy group8.Worker &)
-(cd bin && java -Djava.security.policy=../stupid.policy group8.TestServerForWorker &)
-
-# wait
-# trap "kill 0" SIGINT SIGTERM EXIT
